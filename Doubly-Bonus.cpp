fd6b476e81b90332a11f4642369300ebcc651b75
Ahmed0Ehab2405 <163819547+Ahmed0Ehab2405@users.noreply.github.com>
2025-04-06 13:10:52 +0200

    Create Doubly-Bonus.cpp

---
+++ Doubly-Bonus.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+struct DNode {
+    int data;
+    DNode* prev;
+    DNode* next;
+};
+
+class DoublySortedList {
+    DNode* head;
+
+public:
+    DoublySortedList() {
+        head = nullptr;
+    }
+
+    void insert(int value) {
+        DNode* newNode = new DNode{value, nullptr, nullptr};
+        if (!head || value < head->data) {
+            newNode->next = head;
+            if (head) head->prev = newNode;
+            head = newNode;
+            return;
+        }
+        DNode* current = head;
+        while (current->next && current->next->data < value) {
+            current = current->next;
+        }
+        newNode->next = current->next;
+        if (current->next) current->next->prev = newNode;
+        current->next = newNode;
+        newNode->prev = current;
+    }
+
+    void makenull() {
+        DNode* temp;
+        while (head) {
+            temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    void display() {
+        DNode* current = head;
+        while (current) {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+};
