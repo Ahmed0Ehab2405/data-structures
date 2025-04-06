a0fe9947ff83d7bef7f8b4f66f81844f724ec25c
Ahmed0Ehab2405 <163819547+Ahmed0Ehab2405@users.noreply.github.com>
2025-04-06 13:10:00 +0200

    Create Singly-Bonus.cpp

---
+++ Singly-Bonus.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+class SinglySortedList {
+    Node* head;
+
+public:
+    SinglySortedList() {
+        head = nullptr;
+    }
+
+    void insert(int value) {
+        Node* newNode = new Node{value, nullptr};
+        if (!head || value < head->data) {
+            newNode->next = head;
+            head = newNode;
+            return;
+        }
+        Node* current = head;
+        while (current->next && current->next->data < value) {
+            current = current->next;
+        }
+        newNode->next = current->next;
+        current->next = newNode;
+    }
+
+    void makenull() {
+        Node* temp;
+        while (head) {
+            temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    void display() {
+        Node* current = head;
+        while (current) {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+};
